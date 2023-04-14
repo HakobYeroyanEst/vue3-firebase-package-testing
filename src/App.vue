951bbb0c7341fd3023adf48d6718f8162e56bdd6
HakobYeroyanEst <hakob.yeroyan1@esterox.am>
2023-04-14 16:00:00 +0400

    app init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <p>aaaaaaaaa</p>
+      <RouterView/>
+      <p>bbbbbbbbb</p>
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<template>
+    <p>Hello There</p>
+</template>
+
+<script setup>
+</script>
